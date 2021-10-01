4ab66233053bae0fd6ec01a4d1e70d53d7caad33
Julian Hofmann <julian.hofmann2805@gmail.com>
2021-10-01 14:42:45 +0200

    Add isValidEmail tests

--- handlers/auth_test.go
+++ handlers/auth_test.go
@@ -87,3 +87,29 @@ func TestRegister(t *testing.T) {
 		})
 	}
 }
+
+func Test_isValidEmail(t *testing.T) {
+	tests := []struct {
+		name  string
+		email string
+		want  bool
+	}{
+		{"Valid gmail email", "test@gmail.com", true},
+		{"Valid gmx email", "test@gmx.de", true},
+		{"Valid outlook email", "test@outlook.com", true},
+		{"Valid protonmail email", "test@protonmail.com", true},
+		{"Empty string", "", false},
+		{"Missing @ sign", "test.gmail.com", false},
+		{"Missing name", "@gmail.com", false},
+		{"Missing domain", "test@com", false},
+		{"Non-existant domain", "test@foomail.abc", false},
+		{"Two @ signs", "test@foomail@abc", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isValidEmail(tt.email); got != tt.want {
+				t.Errorf("isValidEmail() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
