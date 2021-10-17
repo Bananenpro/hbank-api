48694d01f244b85e80c1e9e07b6906ccceda34a1
Julian Hofmann <julian.hofmann2805@gmail.com>
2021-10-17 15:50:44 +0200

    Translate jwt auth error messages

--- router/middlewares/jwt.go
+++ router/middlewares/jwt.go
@@ -10,27 +10,19 @@ import (
 
 func JWT(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		lang := c.Get("lang").(string)
 		authToken, err := c.Cookie("Auth-Token")
 		if err != nil {
-			return c.JSON(http.StatusUnauthorized, responses.Base{
-				Success: false,
-				Message: "Missing Auth-Token Cookie",
-			})
+			return c.JSON(http.StatusUnauthorized, responses.New(false, "Missing Auth-Token Cookie", lang))
 		}
 		authTokenSignature, err := c.Cookie("Auth-Token-Signature")
 		if err != nil {
-			return c.JSON(http.StatusUnauthorized, responses.Base{
-				Success: false,
-				Message: "Missing Auth-Token-Signature Cookie",
-			})
+			return c.JSON(http.StatusUnauthorized, responses.New(false, "Missing Auth-Token-Signature Cookie", lang))
 		}
 
 		userId, valid := services.VerifyAuthToken(authToken.Value + "." + authTokenSignature.Value)
 		if !valid {
-			return c.JSON(http.StatusUnauthorized, responses.Base{
-				Success: false,
-				Message: "Invalid JWT",
-			})
+			return c.JSON(http.StatusUnauthorized, responses.New(false, "Invalid JWT", lang))
 		}
 
 		c.Set("userId", userId)
