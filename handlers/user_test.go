8b4acbcb8f1231e977b36bf17d21c9f9a2f74237
Julian Hofmann <julian.hofmann2805@gmail.com>
2021-11-20 15:53:37 +0100

    Add user handler tests

--- handlers/user_test.go
+++ handlers/user_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -8,9 +9,11 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Bananenpro/hbank-api/bindings"
 	"github.com/Bananenpro/hbank-api/config"
 	"github.com/Bananenpro/hbank-api/db"
 	"github.com/Bananenpro/hbank-api/models"
+	"github.com/Bananenpro/hbank-api/responses"
 	"github.com/Bananenpro/hbank-api/router"
 	"github.com/Bananenpro/hbank-api/services"
 	"github.com/google/uuid"
@@ -18,6 +21,81 @@ import (
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/bcrypt"
 )
+
+func TestHandler_GetUsers(t *testing.T) {
+	config.Data.Debug = true
+	r := router.New()
+
+	database, err := db.NewTestDB()
+	if err != nil {
+		t.Fatalf("Couldn't create in memory database")
+	}
+	defer db.DeleteTestDB()
+	err = db.AutoMigrate(database)
+	if err != nil {
+		t.Fatalf("Couldn't auto migrate database")
+	}
+
+	us := db.NewUserStore(database)
+
+	user1 := &models.User{
+		Name:            "bob",
+		Email:           "bob@gmail.com",
+		EmailConfirmed:  true,
+		TwoFaOTPEnabled: true,
+	}
+	us.Create(user1)
+
+	user2 := &models.User{
+		Name:            "peter",
+		Email:           "peter@gmail.com",
+		EmailConfirmed:  true,
+		TwoFaOTPEnabled: true,
+	}
+	us.Create(user2)
+
+	handler := New(us, nil)
+
+	tests := []struct {
+		tName         string
+		user          *models.User
+		pageSize      int
+		includeSelf   bool
+		wantCode      int
+		wantSuccess   bool
+		wantAllInfo   bool
+		wantUserCount int
+	}{
+		{tName: "All", user: user1, pageSize: 10, includeSelf: true, wantCode: http.StatusOK, wantSuccess: true, wantUserCount: 2},
+		{tName: "Don't include self", user: user1, pageSize: 10, includeSelf: false, wantCode: http.StatusOK, wantSuccess: true, wantUserCount: 1},
+		{tName: "Only 1 user", user: user1, pageSize: 1, includeSelf: true, wantCode: http.StatusOK, wantSuccess: true, wantUserCount: 1},
+		{tName: "Invalid page size", user: user1, pageSize: -1, includeSelf: true, wantCode: http.StatusBadRequest, wantSuccess: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.tName, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/?pageSize=%d&includeSelf=%t", tt.pageSize, tt.includeSelf), nil)
+			rec := httptest.NewRecorder()
+			c := r.NewContext(req, rec)
+			c.Set("lang", "en")
+
+			c.Set("userId", tt.user.Id)
+
+			err := handler.GetUsers(c)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantCode, rec.Code)
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"success":%t`, tt.wantSuccess))
+
+			type usersResp struct {
+				responses.Base
+				Users []models.User `json:"users"`
+			}
+			var users usersResp
+			json.Unmarshal([]byte(rec.Body.String()), &users)
+			assert.Equal(t, tt.wantUserCount, len(users.Users))
+		})
+	}
+}
 
 func TestHandler_GetUser(t *testing.T) {
 	config.Data.Debug = true
@@ -335,6 +413,358 @@ func TestHandler_DeleteUserByConfirmEmailCode(t *testing.T) {
 				assert.Nil(t, user)
 			} else if tt.tName != "Wrong id" {
 				assert.NotNil(t, user)
+			}
+		})
+	}
+}
+
+func TestHandler_UpdateUser(t *testing.T) {
+	config.Data.Debug = true
+	r := router.New()
+
+	database, err := db.NewTestDB()
+	if err != nil {
+		t.Fatalf("Couldn't create in memory database")
+	}
+	defer db.DeleteTestDB()
+	err = db.AutoMigrate(database)
+	if err != nil {
+		t.Fatalf("Couldn't auto migrate database")
+	}
+
+	us := db.NewUserStore(database)
+
+	user1 := &models.User{
+		Name:                    "bob",
+		Email:                   "bob@gmail.com",
+		DontSendInvitationEmail: true,
+		ProfilePicturePrivacy:   "hi",
+	}
+	us.Create(user1)
+
+	user2 := &models.User{
+		Name:                    "bob2",
+		Email:                   "bob2@gmail.com",
+		DontSendInvitationEmail: true,
+		ProfilePicturePrivacy:   "hi",
+	}
+	us.Create(user2)
+
+	handler := New(us, nil)
+
+	tests := []struct {
+		tName                   string
+		user                    *models.User
+		dontSendInvitationEmail bool
+		profilePicturePrivacy   string
+		wantCode                int
+		wantSuccess             bool
+		wantMessage             string
+	}{
+		{tName: "Success", user: user1, dontSendInvitationEmail: false, profilePicturePrivacy: "everybody", wantCode: http.StatusOK, wantSuccess: true},
+		{tName: "Invalid profilePicturePrivacy", user: user2, dontSendInvitationEmail: false, profilePicturePrivacy: "blablabla", wantCode: http.StatusOK, wantSuccess: false, wantMessage: "Invalid profile picture privacy"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.tName, func(t *testing.T) {
+			jsonBody := fmt.Sprintf(`{"dontSendInvitationEmail": %t, "profilePicturePrivacy": "%s", "email": "bla@bla.bla", "password": "123456"}`, tt.dontSendInvitationEmail, tt.profilePicturePrivacy)
+			req := httptest.NewRequest(http.MethodPut, "/", strings.NewReader(jsonBody))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := r.NewContext(req, rec)
+			c.Set("lang", "en")
+
+			c.Set("userId", tt.user.Id)
+
+			err := handler.UpdateUser(c)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantCode, rec.Code)
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"success":%t`, tt.wantSuccess))
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"message":"%s"`, tt.wantMessage))
+
+			user, _ := us.GetById(tt.user.Id)
+			if tt.wantSuccess {
+				assert.Equal(t, tt.dontSendInvitationEmail, user.DontSendInvitationEmail)
+				assert.Equal(t, tt.profilePicturePrivacy, user.ProfilePicturePrivacy)
+			} else {
+				assert.NotEqual(t, tt.dontSendInvitationEmail, user.DontSendInvitationEmail)
+				assert.NotEqual(t, tt.profilePicturePrivacy, user.ProfilePicturePrivacy)
+			}
+
+			assert.Equal(t, tt.user.Email, user.Email)
+			assert.Equal(t, tt.user.PasswordHash, user.PasswordHash)
+		})
+	}
+}
+
+func TestHandler_GetCurrentCash(t *testing.T) {
+	config.Data.Debug = true
+	r := router.New()
+
+	database, err := db.NewTestDB()
+	if err != nil {
+		t.Fatalf("Couldn't create in memory database")
+	}
+	defer db.DeleteTestDB()
+	err = db.AutoMigrate(database)
+	if err != nil {
+		t.Fatalf("Couldn't auto migrate database")
+	}
+
+	us := db.NewUserStore(database)
+
+	user1 := &models.User{
+		Name:  "bob",
+		Email: "bob@gmail.com",
+		CashLog: []models.CashLogEntry{
+			{ChangeTitle: "Change1", Base: models.Base{Created: time.Now().Unix()}},
+			{ChangeTitle: "Change2", Base: models.Base{Created: time.Now().Unix() + 100000}},
+			{ChangeTitle: "Change3", Base: models.Base{Created: time.Now().Unix()}},
+		},
+	}
+	us.Create(user1)
+
+	user2 := &models.User{
+		Name:  "peter",
+		Email: "peter@gmail.com",
+	}
+	us.Create(user2)
+
+	handler := New(us, nil)
+
+	tests := []struct {
+		tName       string
+		user        *models.User
+		wantCode    int
+		wantSuccess bool
+		wantMessage string
+		wantTitle   string
+	}{
+		{tName: "Success", user: user1, wantCode: http.StatusOK, wantSuccess: true, wantTitle: "Change2"},
+		{tName: "Empty cash log", user: user2, wantCode: http.StatusNotFound, wantSuccess: false, wantMessage: "Cash log is empty"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.tName, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rec := httptest.NewRecorder()
+			c := r.NewContext(req, rec)
+			c.Set("lang", "en")
+			c.Set("userId", tt.user.Id)
+
+			err := handler.GetCurrentCash(c)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantCode, rec.Code)
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"success":%t`, tt.wantSuccess))
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"message":"%s"`, tt.wantMessage))
+
+			if tt.wantSuccess {
+				assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"title":"%s"`, tt.wantTitle))
+			}
+		})
+	}
+}
+
+func TestHandler_GetCashLogEntryById(t *testing.T) {
+	config.Data.Debug = true
+	r := router.New()
+
+	database, err := db.NewTestDB()
+	if err != nil {
+		t.Fatalf("Couldn't create in memory database")
+	}
+	defer db.DeleteTestDB()
+	err = db.AutoMigrate(database)
+	if err != nil {
+		t.Fatalf("Couldn't auto migrate database")
+	}
+
+	us := db.NewUserStore(database)
+
+	user := &models.User{
+		Name:  "bob",
+		Email: "bob@gmail.com",
+		CashLog: []models.CashLogEntry{
+			{ChangeTitle: "Change1", Base: models.Base{Created: time.Now().Unix()}},
+			{ChangeTitle: "Change2", Base: models.Base{Created: time.Now().Unix()}},
+			{ChangeTitle: "Change3", Base: models.Base{Created: time.Now().Unix()}},
+		},
+	}
+	us.Create(user)
+
+	handler := New(us, nil)
+
+	tests := []struct {
+		tName       string
+		entryId     uuid.UUID
+		wantCode    int
+		wantSuccess bool
+		wantMessage string
+		wantTitle   string
+	}{
+		{tName: "Success", entryId: user.CashLog[2].Id, wantCode: http.StatusOK, wantSuccess: true, wantTitle: "Change3"},
+		{tName: "Doesn't exist", entryId: uuid.New(), wantCode: http.StatusNotFound, wantSuccess: false, wantMessage: "Resource not found"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.tName, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			rec := httptest.NewRecorder()
+			c := r.NewContext(req, rec)
+			c.Set("lang", "en")
+			c.Set("userId", user.Id)
+			c.SetParamNames("id")
+			c.SetParamValues(tt.entryId.String())
+
+			err := handler.GetCashLogEntryById(c)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantCode, rec.Code)
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"success":%t`, tt.wantSuccess))
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"message":"%s"`, tt.wantMessage))
+
+			if tt.wantSuccess {
+				assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"title":"%s"`, tt.wantTitle))
+			}
+		})
+	}
+}
+
+func TestHandler_GetCashLog(t *testing.T) {
+	config.Data.Debug = true
+	r := router.New()
+
+	database, err := db.NewTestDB()
+	if err != nil {
+		t.Fatalf("Couldn't create in memory database")
+	}
+	defer db.DeleteTestDB()
+	err = db.AutoMigrate(database)
+	if err != nil {
+		t.Fatalf("Couldn't auto migrate database")
+	}
+
+	us := db.NewUserStore(database)
+
+	user := &models.User{
+		Name:  "bob",
+		Email: "bob@gmail.com",
+		CashLog: []models.CashLogEntry{
+			{ChangeTitle: "Change1", Base: models.Base{Created: time.Now().Unix()}},
+			{ChangeTitle: "Change2", Base: models.Base{Created: time.Now().Unix()}},
+			{ChangeTitle: "Change3", Base: models.Base{Created: time.Now().Unix()}},
+		},
+	}
+	us.Create(user)
+
+	handler := New(us, nil)
+
+	tests := []struct {
+		tName       string
+		pageSize    int
+		wantCode    int
+		wantSuccess bool
+		wantMessage string
+		wantCount   int
+	}{
+		{tName: "Get all", pageSize: 10, wantCode: http.StatusOK, wantSuccess: true, wantCount: 3},
+		{tName: "Get 1", pageSize: 1, wantCode: http.StatusOK, wantSuccess: true, wantCount: 1},
+		{tName: "Invalid pageSize", pageSize: -1, wantCode: http.StatusBadRequest, wantSuccess: false, wantMessage: "Unsupported page size"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.tName, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/?pageSize=%d", tt.pageSize), nil)
+			rec := httptest.NewRecorder()
+			c := r.NewContext(req, rec)
+			c.Set("lang", "en")
+			c.Set("userId", user.Id)
+			c.SetParamNames("id")
+
+			err := handler.GetCashLog(c)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantCode, rec.Code)
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"success":%t`, tt.wantSuccess))
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"message":"%s"`, tt.wantMessage))
+
+			if tt.wantSuccess {
+				type cashLogResp struct {
+					responses.Base
+					CashLog []models.CashLogEntry `json:"log"`
+				}
+				var resp cashLogResp
+				json.Unmarshal([]byte(rec.Body.String()), &resp)
+				assert.Equal(t, tt.wantCount, len(resp.CashLog))
+			}
+		})
+	}
+}
+
+func TestHandler_AddCashLogEntry(t *testing.T) {
+	config.Data.Debug = true
+	r := router.New()
+
+	database, err := db.NewTestDB()
+	if err != nil {
+		t.Fatalf("Couldn't create in memory database")
+	}
+	defer db.DeleteTestDB()
+	err = db.AutoMigrate(database)
+	if err != nil {
+		t.Fatalf("Couldn't auto migrate database")
+	}
+
+	us := db.NewUserStore(database)
+
+	user1 := &models.User{
+		Name:  "bob",
+		Email: "bob@gmail.com",
+	}
+	us.Create(user1)
+
+	user2 := &models.User{
+		Name:  "bob2",
+		Email: "bob2@gmail.com",
+	}
+	us.Create(user2)
+
+	handler := New(us, nil)
+
+	tests := []struct {
+		tName       string
+		user        *models.User
+		entry       bindings.AddCashLogEntry
+		wantCode    int
+		wantSuccess bool
+		wantMessage string
+	}{
+		{tName: "Success", user: user1, entry: bindings.AddCashLogEntry{Title: "Test"}, wantCode: http.StatusCreated, wantSuccess: true, wantMessage: "Successfully added new cash log entry"},
+		{tName: "Title too short", user: user2, entry: bindings.AddCashLogEntry{Title: "    hi   "}, wantCode: http.StatusOK, wantSuccess: false, wantMessage: "Title too short"},
+		{tName: "Title too long", user: user2, entry: bindings.AddCashLogEntry{Title: "123456789012345678901"}, wantCode: http.StatusOK, wantSuccess: false, wantMessage: "Title too long"},
+		{tName: "Description too long", user: user2, entry: bindings.AddCashLogEntry{Title: "Test", Description: strings.Repeat("a", 257)}, wantCode: http.StatusOK, wantSuccess: false, wantMessage: "Description too long"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.tName, func(t *testing.T) {
+			jsonBody, _ := json.Marshal(tt.entry)
+			req := httptest.NewRequest(http.MethodPut, "/", strings.NewReader(string(jsonBody)))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := r.NewContext(req, rec)
+			c.Set("lang", "en")
+
+			c.Set("userId", tt.user.Id)
+
+			err := handler.AddCashLogEntry(c)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.wantCode, rec.Code)
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"success":%t`, tt.wantSuccess))
+			assert.Contains(t, rec.Body.String(), fmt.Sprintf(`"message":"%s"`, tt.wantMessage))
+
+			log, _ := us.GetCashLog(tt.user, 0, 10, false)
+			if tt.wantSuccess {
+				assert.Equal(t, 1, len(log))
+			} else {
+				assert.Equal(t, 0, len(log))
 			}
 		})
 	}
