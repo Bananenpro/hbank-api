20dc11c26488180f11bfa587f56e8a3a1aad780f
Julian Hofmann <julian.hofmann2805@gmail.com>
2021-11-20 17:33:18 +0100

    Add 1 as true value for query params

--- services/utils.go
+++ services/utils.go
@@ -5,5 +5,5 @@ import "strings"
 func StrToBool(value string) bool {
 	return strings.EqualFold(value, "true") || strings.EqualFold(value, "t") ||
 		strings.EqualFold(value, "yes") || strings.EqualFold(value, "y") ||
-		strings.EqualFold(value, "on")
+		strings.EqualFold(value, "on") || strings.EqualFold(value, "1")
 }
