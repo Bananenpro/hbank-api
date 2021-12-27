7c95ee4dd422e043b89a3d0dd9e651973be416e5
Julian Hofmann <julian.hofmann2805@gmail.com>
2021-12-27 15:19:44 +0100

    Improve picture too big response

--- services/utils.go
+++ services/utils.go
@@ -1,9 +1,24 @@
 package services
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func StrToBool(value string) bool {
 	return strings.EqualFold(value, "true") || strings.EqualFold(value, "t") ||
 		strings.EqualFold(value, "yes") || strings.EqualFold(value, "y") ||
 		strings.EqualFold(value, "on") || strings.EqualFold(value, "1")
+}
+
+func SizeInBytesToStr(size int64) string {
+	if size >= 1000000000 {
+		return fmt.Sprintf("%d GB", size/1000000000)
+	} else if size >= 1000000 {
+		return fmt.Sprintf("%d MB", size/1000000)
+	} else if size >= 1000 {
+		return fmt.Sprintf("%d kB", size/1000)
+	} else {
+		return fmt.Sprintf("%d B", size)
+	}
 }
